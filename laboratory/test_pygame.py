fd5b036c980b84e3322192def476cc309d12c3ff
cabbet <cabbetlong@gmail.com>
2018-09-26 15:26:21 +0800

    update the code which demonstrate how to play mp3 use pygame

--- laboratory/test_pygame.py
+++ laboratory/test_pygame.py
@@ -1,7 +1,41 @@
 import pygame
 
-file = 'test.mp3'
-pygame.init()
-pygame.mixer.init()
-pygame.mixer.music.load(file)
-pygame.event.wait()
+def pmusic(file):
+    pygame.init()
+    pygame.mixer.init()
+    clock = pygame.time.Clock()
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        print('Playing...')
+        clock.tick(1000)
+
+
+def stopmusic():
+    pygame.mixer.music.stop()
+
+
+def getmixerargs():
+    pygame.mixer.init()
+    freq, size, chan = pygame.mixer.get_init()
+    return freq, size, chan
+
+
+def initMixer():
+    BUFFER = 3072
+    FREQ, SIZE, CHAN = getmixerargs()
+    pygame.mixer.init(FREQ, SIZE, CHAN, BUFFER)
+
+
+def main():
+    try:
+        initMixer()
+        file = 'test.mp3'
+        pmusic(file)
+    except KeyboardInterrupt:
+        stopmusic()
+        print('\nPlay Stopped by user')
+
+
+if __name__ == '__main__':
+    main()
